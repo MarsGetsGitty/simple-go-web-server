329b0a24ddfc70a8bc538a365172c2a5259d80b9
echorza <91528909+MarsGetsGitty@users.noreply.github.com>
2025-08-20 23:53:21 -0400

    Add POST /users endpoint to create users
    Introduces a new HTTP handler for POST /users that accepts a JSON payload to create a user. User data is validated and stored in an in-memory map simulating a user database.

--- main.go
+++ main.go
@@ -1,14 +1,23 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 )
+
+type User struct {
+	Name string `json:"name"`
+}
+
+var userCache = make(map[int]User) // create map to sim a user db
 
 // main heap
 func main() {
 	mux := http.NewServeMux()       // creates a new request multiplexer
 	mux.HandleFunc("/", handleRoot) // template used to control the traffic
+
+	mux.HandleFunc("POST /users", createUser)
 
 	// at this point, the server as not started yet.
 
@@ -22,4 +31,23 @@ func handleRoot(
 	r *http.Request, // contains information of the request; (example: body; headers; url;)
 ) {
 	fmt.Fprintf(w, "Hello World.")
+}
+
+func createUser(w http.ResponseWriter, r *http.Request) {
+	var user User
+	// (1) create a new decoder based off body info in the request. (2) decode this body info for the user
+	err := json.NewDecoder(r.Body).Decode(&user)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	// (3) validate username
+	if user.Name == "" {
+		http.Error(w, "username is required", http.StatusBadRequest)
+		return
+	}
+	// (4) new id; insert user into local memory db
+	userCache[len(userCache)+1] = user
+
+	w.WriteHeader(http.StatusNoContent)
 }
